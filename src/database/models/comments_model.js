34dde034033a33a2791638d69f84439a8aa70c9e
AstraSama <fe_lipe-mar_tins@hotmail.com>
2025-01-09 18:13:16 -0300

    mudança diagrama, add comentários

--- src/database/models/comments_model.js
+++ src/database/models/comments_model.js
@@ -1,5 +1,21 @@
 import { Schema, model } from "mongoose";
 
+const comment = new Schema({
+    user: {
+        type: Schema.ObjectId,
+        ref: "User",
+        required: true
+    },
+    text: {
+        type: String,
+        required: true
+    }
+});
+
 const comments_Schema = new Schema({
-    //TODO colocar atributos
-})
+    comments: [comment]
+});
+
+const Comments = model("Comments", comments_Schema);
+
+export default Comments;
